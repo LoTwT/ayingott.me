2d0cf1b177bd463e0ff93ea1f096a77aa4d055ac
LoTwT <709937065@qq.com>
2022-05-31 20:46:00 +0800

    chore: init

---
+++ app.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+useHead({
+  title: "Nuxt3 starter",
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/nuxt.png",
+    },
+  ],
+})
+</script>
+
+<template>
+  <main class="py-20 px-10 text-center">
+    <NuxtPage />
+    <Footer />
+  </main>
+</template>
+
+<style>
+html,
+body,
+#__nuxt {
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+html.dark {
+  background: #222;
+  color: white;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+import { defineNuxtConfig } from "nuxt"
+
+export default defineNuxtConfig({
+  modules: [
+    "@vueuse/nuxt",
+    "@unocss/nuxt",
+    "@pinia/nuxt",
+    "@nuxtjs/color-mode",
+  ],
+  experimental: {
+    reactivityTransform: true,
+  },
+  unocss: {
+    preflight: true,
+  },
+  colorMode: {
+    classSuffix: "",
+  },
+})
