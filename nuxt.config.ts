5b1eddea4b68cf0f9ae2c6a7b8df25343626ddb1
LoTwT <709937065@qq.com>
2022-06-15 08:19:12 +0800

    chore: add @vueuse/motion

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     "@unocss/nuxt",
     "@pinia/nuxt",
     "@nuxtjs/color-mode",
+    "@vueuse/motion/nuxt",
   ],
   experimental: {
     reactivityTransform: true,
