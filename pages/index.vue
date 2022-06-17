6da7607eaf89979e02d01b7924b656875d55327a
LoTwT <709937065@qq.com>
2022-06-17 19:01:41 +0800

    feat: a-lint transition

--- pages/index.vue
+++ pages/index.vue
@@ -5,41 +5,21 @@
     <img src="/mea.jpg" alt="avatar" rounded-full />
   </MotionAppear>
   <MotionAppear :duration="5000" mb-10>
-    <span text-5xl opacity-80> Hi! I'm an ordinary FE developer. </span>
+    <span text-5xl opacity-80> Hi, I'm an ordinary FE developer</span>
   </MotionAppear>
   <MotionAppear :duration="5000">
     <span text-5xl opacity-80>
       Find me on
       <NuxtLink href="https://github.com/lotwt">
-        <span class="github" relative>Github</span>
+        <span
+          relative
+          before="content-none w-9 h-1 absolute bg-black bottom-0 left-0 transition-all duration-500"
+          before:dark:bg-white
+          hover:before="w-100%"
+        >
+          Github
+        </span>
       </NuxtLink>
-      .
     </span>
   </MotionAppear>
 </template>
-
-<style scoped>
-.github::before {
-  content: "";
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 5px;
-  background-color: white;
-  z-index: -1;
-  transition: all 0.5s;
-}
-
-.github:hover::before {
-  height: 100%;
-}
-
-.github:hover {
-  color: black;
-}
-
-.github:active:before {
-  background: #b9b9b9;
-}
-</style>
