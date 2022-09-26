70ac2d60d1672149f9febcc3f8c43ca52d27036e
LoTwT <709937065@qq.com>
2022-09-26 23:02:33 +0800

    chore: migrate to vitesse

---
+++ src/layouts/intro.vue
@@ -0,0 +1,8 @@
+<template>
+  <main h-full flex flex-col px-20 pt-10>
+    <div flex-1>
+      <RouterView />
+    </div>
+    <Footer />
+  </main>
+</template>
---
+++ src/pages/intro.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div relative w-full h-full flex md:flex-row flex-col>
+    <div flex-1>
+      <div text-12 border-b-1>
+        Hi!
+      </div>
+      <div text-6 pt-8>
+        <div>I'm Ayingott, you can also call me LoTwT.</div>
+        <br>
+
+        <div>
+          I'm just an ordinary developer interested in JavaScript / TypeScript.
+        </div>
+        <br>
+
+        <div>
+          Find me on
+          <FindLink title="Github" href="https://github.com/lotwt" />,
+          <FindLink title="Twitter" href="https://twitter.com/ayingott319" />.
+        </div>
+        <br>
+
+        <div>Mail me at <span border-b-1>l709937065@gmail.com</span> .</div>
+        <br>
+
+        <div>Hope and looking for a remote job.</div>
+      </div>
+    </div>
+
+    <div flex justify-center items-center text-center>
+      <Squares />
+    </div>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: intro
+</route>
+
