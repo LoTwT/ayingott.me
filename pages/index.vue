7abc7b182a38df654dcff1f6cc3421a8cfb937e0
LoTwT <709937065@qq.com>
2022-11-28 14:37:44 +0800

    chore: fix verbose <div>

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-    <Suspense>
-      <div h-full flex flex-col justify-center items-center>
-        <div text-4xl>Hi, I'm Lo.</div>
-        <Loader mt-10 />
+  <Suspense>
+    <div h-full flex flex-col justify-center items-center>
+      <div text-4xl>Hi, I'm Lo.</div>
+      <Loader mt-10 />
+    </div>
+    <template #fallback>
+      <div op50 italic>
+        <span animate-pulse>Loading...</span>
       </div>
-      <template #fallback>
-        <div op50 italic>
-          <span animate-pulse>Loading...</span>
-        </div>
-      </template>
-    </Suspense>
-  </div>
+    </template>
+  </Suspense>
 </template>
