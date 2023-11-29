b17159a262a687e12dc497f0cd6968fd32a66424
LoTwT <709937065@qq.com>
2023-11-29 23:48:17 +0800

    fix: try to fix prod hydration mismatch

--- pages/resume.vue
+++ pages/resume.vue
@@ -1,11 +1,5 @@
-<script setup lang="ts">
-const { data } = await useAsyncData("resume", () =>
-  queryContent("/resume").findOne(),
-)
-</script>
-
 <template>
   <div max-w-none px-8 prose lg="max-w-80ch px-0">
-    <ContentRenderer :value="data ?? {}" />
+    <ContentDoc path="/resume" />
   </div>
 </template>
