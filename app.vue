b082fab9e1cfffed83be4ed723513dd6f8c75ba9
Lo <l709937065@gmail.com>
2024-03-26 17:45:19 +0800

    chore: update meta

--- app.vue
+++ app.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+useHead({
+  title: "Ayingott",
+  meta: [
+    {
+      name: "description",
+      content: "Ayingott's website",
+    },
+  ],
+})
+
+useSeoMeta({
+  title: "Ayingott",
+  ogTitle: "Ayingott's website",
+  description: "Ayingott's website",
+  ogDescription: "Ayingott's website",
+  // ogImage: "",
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
